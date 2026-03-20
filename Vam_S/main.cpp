8033c74b966a9a47dda2b686ee363aa9c92fd4e9
j0hnyk <153216339+WorkLoopC@users.noreply.github.com>
2026-03-20 10:19:41 +0100

    Update main.cpp

--- Vam_S/main.cpp
+++ Vam_S/main.cpp
@@ -14,14 +14,12 @@ int main()
     sf::Clock clock;
     sf::Texture texture;
     sf::Sprite sprite;
-
-
     sf::RenderWindow window(sf::VideoMode(800, 600), "Demo");
     window.setFramerateLimit(60);
+
     if (!texture.loadFromFile("image.jpg")) std::cout << "FAILED TO LOAD\n";
-    //sprite.setTexture(texture);
-    Animation animation (&player.m_playerCharacter, sf::Vector2u (3,6), 0.3f);
-    sprite.setTextureRect(animation.Rect);
+    sprite.setTexture(texture);
+    Animation animation(&player.m_playerCharacter, sf::Vector2u(3, 2), 0.3f);
     while (window.isOpen()) {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
@@ -30,26 +28,24 @@ int main()
         }
         float dt = clock.restart().asSeconds();
 
+        animation.update(0, dt);
+        player.m_rect.setTextureRect(animation.Rect);
+
         player.update(dt, window);
         player.player_position_update(window);
 
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left) ) {  //&& cl.getElapsedTime().asSeconds() > 0.5
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {  //&& cl.getElapsedTime().asSeconds() > 0.5
             player.shoot_mouse(dt, window);
         }
-        
-        
-        animation.update(0,dt);
-
         window.clear();
         window.draw(sprite);
-        
+        player.draw(window);
         int i{};
         for (auto& p : player.m_new_projectile) {
             p.update(player.m_angle[i]);
             p.draw(window);
             i++;
         }
-        player.draw(window);
         window.display();
     }
     return 0;
